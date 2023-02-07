ea9dd3e3e1cb3edf2ca3d9c80620e48cd3128559
Vlad-Avoyan <avoyanvlad00@gmail.com>
2023-02-07 21:38:17 +0400

    testing DB methods

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::where('is_published', 1)->first();
+            dump($posts->title);
+        dd('end');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [MyPlaceController::class, "sayHello"]);
+Route::get('/posts', [PostController::class, "index"]);
